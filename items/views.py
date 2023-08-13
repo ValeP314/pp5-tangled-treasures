ace5f9cf0b06234292a95d9b8abd0e2832c198f3
ValeP314 <>
2023-08-13 10:43:11 +0000

    Added item details functionality

--- items/views.py
+++ items/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 # Create your views here.
+
 
 def all_items(request):
     """ A view to show all products, including sorting and search queries """
@@ -13,3 +14,15 @@ def all_items(request):
     }
 
     return render(request, 'items/items.html', context)
+
+
+def item_detail(request, item_id):
+    """ A view to show individual item details """
+
+    item = get_object_or_404(Item, pk=item_id)
+
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'items/item_detail.html', context)
