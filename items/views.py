01c9fd0e05e02d936fa7666b7affd313fd0d8b61
ValeP314 <>
2023-08-13 13:41:20 +0000

    Added sorting

--- items/views.py
+++ items/views.py
@@ -7,14 +7,28 @@ from .models import Item, Category
 
 
 def all_items(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all items, including sorting and search queries """
 
     items = Item.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
-        
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                items = items.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            items = items.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             items = items.filter(category__name__in=categories)
@@ -31,10 +45,13 @@ def all_items(request):
                 description__icontains=query)
             items = items.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'items': items,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'items/items.html', context)
